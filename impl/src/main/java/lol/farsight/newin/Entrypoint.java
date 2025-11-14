f99a7880ec63a7121f7e9cbcbaa5accf2140702e
yearbarber <66645533+yearbarber@users.noreply.github.com>
2025-11-14 21:59:40 +0300

    build!: rename subprojects

--- impl/src/main/java/lol/farsight/newin/Entrypoint.java
+++ impl/src/main/java/lol/farsight/newin/Entrypoint.java
@@ -1,11 +1,8 @@
 package lol.farsight.newin;
 
 import lol.farsight.newin.registrar.Newins;
-import me.lucko.spark.paper.lib.protobuf.Mixin;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class Entrypoint extends JavaPlugin {
     @Override
