d4e00e8f0a316aaa7ccb826de59268d532d196b3
MrLarkyy_ <studiotadikl@gmail.com>
2025-12-08 22:05:04 +0100

    Migrate build scripts to Kotlin DSL for consistency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly "org.jetbrains:annotations:26.0.0"
+    compileOnly("org.jetbrains:annotations:26.0.0")
 
     implementation("org.ow2.asm:asm:9.9")
     implementation("org.ow2.asm:asm-commons:9.9")
--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 repositories {
     maven {
         name = "papermc"
-        url = "https://repo.papermc.io/repository/maven-public/"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
 dependencies {
-    compileOnly "io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT"
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
 
     api(project(":newin-core"))
 }
